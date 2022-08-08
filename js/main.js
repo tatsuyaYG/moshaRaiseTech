a8e77728c15f5359aef8dd0cc6bda4eaab7c5832
tatsuyaYAGI <yagiappwp@gmail.com>
2022-08-09 00:32:33 +0900

    レスポンシブ対応（メニュー以外）

--- js/main.js
+++ js/main.js
@@ -5,12 +5,10 @@ $(function(){
     $('.first-nav').each(function(){
         $(this).hover(
             function(){
-                console.log('open');
                 $(this).children('.sub-menu').slideDown(300);
             },
             function(){
                 $(this).children('.sub-menu').hide()
-                console.log('hide')
             }
     );
     });
