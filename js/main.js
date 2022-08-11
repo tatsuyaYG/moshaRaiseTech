a63927cd1a9130157650d042e7e2962475b42d68
tatsuyaYAGI <yagiappwp@gmail.com>
2022-08-11 15:31:43 +0900

    完成

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+// ヘッダーメニュー
 $(function(){
     $('.first-nav').each(function(){
         $(this).hover(
@@ -11,5 +11,46 @@ $(function(){
                 $(this).children('.sub-menu').hide()
             }
     );
+    });
+});
+
+// ハンバーガーボタン
+$(function(){
+    var ac = $('#ac-menu')
+    var btn = $('#btn')
+    $('.nav_btn').click(function(){
+        if(ac.hasClass('close')){
+            $('.nav-menu').slideDown(300);
+            ac.removeClass('close');
+            ac.addClass('open');
+            btn.addClass('open');
+            $('.nav_btn').addClass('open');
+            console.log(ac.attr('class'));
+        }
+        else{
+            $('.nav-menu').slideUp(300);
+            ac.removeClass('open');
+            ac.addClass('close');
+            btn.removeClass('open');
+            $('.nav_btn').removeClass('open');
+            console.log(ac.attr('class'));
+        };
+    });
+});
+
+// サブメニューが開いたときの＋ボタン
+$(function(){
+    $('.plus').hover(function(){
+        $(this).toggleClass('open');
+    })
+})
+
+$(function(){
+    $(window).resize(function(){
+        if(window.matchMedia('(max-width:1200px)').matches){
+            $('.nav-menu').css('display','none');
+        }else{
+            $('.nav-menu').css('display','block');
+        };
     });
 });
